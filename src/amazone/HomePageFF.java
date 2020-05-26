7ec2ce5c696d5be946f664db11d8f9780b3009f4
smhrash <smhrash@gmail.com>
2020-05-25 21:21:27 -0400

    HomePageFF updated

--- src/amazone/HomePageFF.java
+++ src/amazone/HomePageFF.java
@@ -1,11 +1,12 @@
 package amazone;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HomePageFF {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
         String firefoxDriver = "webDiver.gecko.driver";
@@ -15,6 +16,14 @@ public class HomePageFF {
 
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.amazon.com/");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys("USB adopter for mac computer");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("/html/body/div[1]/header/div/div[1]/div[3]/div/form/div[2]/div/input")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div/span[4]/div[2]/div[5]/div/span/div/div/div[2]/div[1]/div/div/span/a/div/img")).click();
+
+        Thread.sleep(3000);
 
     }
 }
